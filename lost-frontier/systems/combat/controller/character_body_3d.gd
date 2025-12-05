7010e59de7df68d850795fdf79db9f7abb7d0d1f
VanLoop <kozlovrus123@gmail.com>
2025-12-05 01:58:13 -0500

    Update motion
    smoother motion with mouse

--- lost-frontier/systems/combat/controller/character_body_3d.gd
+++ lost-frontier/systems/combat/controller/character_body_3d.gd
@@ -4,7 +4,7 @@ extends CharacterBody3D
 @export var rotation_speed = 2.5
 @onready var camera := get_viewport().get_camera_3d()
 
-func rotate_toward_mouse():
+func rotate_toward_mouse(delta):
 	var mouse_pos = get_viewport().get_mouse_position()
 	var from = camera.project_ray_origin(mouse_pos)
 	var to = from + camera.project_ray_normal(mouse_pos) * 2000
@@ -22,8 +22,12 @@ func rotate_toward_mouse():
 		dir = dir.normalized()
 		
 		# rotate only around Y-axis
-		var target_rotation = atan2(dir.x, dir.z)
-		rotation.y = target_rotation
+		#var target_rotation = atan2(dir.x, dir.z)
+		#rotation.y = target_rotation
+		
+		# rotate only aroun Y smoother
+		var target_yaw = atan2(dir.x, dir.z)
+		rotation.y = lerp_angle(rotation.y, target_yaw, 8.0 * delta)
 
 func _physics_process(delta):
 	var input_vector = Vector3.ZERO
@@ -47,7 +51,7 @@ func _physics_process(delta):
 	velocity = direction.normalized() * speed
 	move_and_slide()
 	
-	rotate_toward_mouse()
+	rotate_toward_mouse(delta)
 
 	# Rotation (Yaw only)
 	if Input.is_action_pressed("turn_left"):
