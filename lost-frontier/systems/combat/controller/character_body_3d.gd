2bde97ffdccebe16e0c109157375d9c931638453
VanLoop <kozlovrus123@gmail.com>
2025-12-04 22:36:36 -0500

    mouse orientation prototype
    the mouse movement now guide the orientation of the ship, it still needs work but it works.

--- lost-frontier/systems/combat/controller/character_body_3d.gd
+++ lost-frontier/systems/combat/controller/character_body_3d.gd
@@ -2,6 +2,21 @@ extends CharacterBody3D
 
 @export var speed = 15.0
 @export var rotation_speed = 2.5
+@onready var camera := get_viewport().get_camera_3d()
+
+func rotate_toward_mouse():
+	var mouse_pos = get_viewport().get_mouse_position()
+	var from = camera.project_ray_origin(mouse_pos)
+	var to = from + camera.project_ray_normal(mouse_pos) * 2000
+	
+	#create a virtual horizontal plane at the ship height y = globalposition
+	var plane = Plane(Vector3.UP, -global_position.y)
+	var hit_pos = plane.intersects_ray(from, to)
+	
+	if hit_pos:
+		var dir = (hit_pos - global_position).normalized()
+		var look_at_pos = global_position + dir
+		look_at(look_at_pos, Vector3.UP)
 
 func _physics_process(delta):
 	var input_vector = Vector3.ZERO
@@ -24,6 +39,8 @@ func _physics_process(delta):
 
 	velocity = direction.normalized() * speed
 	move_and_slide()
+	
+	rotate_toward_mouse()
 
 	# Rotation (Yaw only)
 	if Input.is_action_pressed("turn_left"):
