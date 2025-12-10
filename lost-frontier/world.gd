db78c6c454c2357e993c320b503423cbd0d4b55b
VanLoop <kozlovrus123@gmail.com>
2025-12-10 10:03:12 -0500

    win message
    win message and game stop when max score is reached

--- lost-frontier/world.gd
+++ lost-frontier/world.gd
@@ -1,19 +1,13 @@
-extends Node
+extends Node3D
 
-var score: int = 0
-signal win
-
-func add_points(points: int):
-	score += points
-	print("Score: ", score)
-	if score >= 100:
-		emit_signal("win")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	ScoreManager.connect("win", Callable(self, "_on_win"))
 
-
+func _on_win():
+	get_tree().paused = true
+	$ui/win_label.visible = true
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
